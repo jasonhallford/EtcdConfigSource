ef667db01c356689e0e360f9cfc59bf18ec4668b
Jason Hallford <jason.hallford@gmail.com>
2019-11-11 14:13:07 -0800

    Added basic watch support.
    Setting a new property, etc.cs.watch, to "true" will cause the config
    source to set an etcd watch for any read property. Should any of those
    properties change, the source will clear the relevant cache entry and
    re-read the value the next time it is requested.

--- src/main/java/net/ghettopalace/etcd/EtcdConfigSource.java
+++ src/main/java/net/ghettopalace/etcd/EtcdConfigSource.java
@@ -2,16 +2,21 @@ package net.ghettopalace.etcd;
 
 import com.google.common.base.Strings;
 import com.google.protobuf.ByteString;
+import com.ibm.etcd.api.Event;
+import com.ibm.etcd.api.KeyValue;
 import com.ibm.etcd.api.RangeResponse;
 import com.ibm.etcd.client.EtcdClient;
 import com.ibm.etcd.client.KvStoreClient;
 import com.ibm.etcd.client.kv.KvClient;
+import com.ibm.etcd.client.kv.WatchUpdate;
+import io.grpc.stub.StreamObserver;
 import org.apache.deltaspike.core.api.config.ConfigResolver;
 import org.apache.deltaspike.core.spi.config.ConfigSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.file.WatchEvent;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +26,46 @@ import java.util.Map;
  * @author Jason Hallford
  */
 public class EtcdConfigSource implements ConfigSource, AutoCloseable {
+    // Inner classes
+
+    /**
+     * A class for managing asynchronous watch updates from etcd.
+     */
+    class WatchObserver implements StreamObserver<WatchUpdate> {
+        // Constructors
+        public WatchObserver() {
+
+        }
+
+        // StreamObserver
+        @Override
+        public void onNext(WatchUpdate watchUpdate) {
+            if (watchUpdate.getEvents() != null) {
+                LOGGER.debug("Processing watch updates...");
+
+                for (Event evt : watchUpdate.getEvents()) {
+                    KeyValue kv = evt.getKv();
+                    String key = kv.getKey().toStringUtf8();
+
+                    LOGGER.debug("Processing event for key '{}'.", key);
+                    removeCachedValue(key);
+                }
+
+                LOGGER.debug("Watch updates processed.");
+            }
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+            LOGGER.error("WatchObserver received as error: ", throwable);
+        }
+
+        @Override
+        public void onCompleted() {
+            LOGGER.debug("onCompleted() invoked for WatchObserver.");
+        }
+    }
+
     // Constants
     private static final String SOURCE_NAME = "Etcd Config Source";
     private static final String ETCD_HOST_PROP = "etcd.endpoint.host";
@@ -28,6 +73,7 @@ public class EtcdConfigSource implements ConfigSource, AutoCloseable {
     private static final String ETCD_USER_PROP = "etcd.endpoint.user";
     private static final String ETCD_PASSWORD_PROP = "etcd.endpoint.password";
     private static final String ORDINAL_PROP = "etcd.cs.ordinal";
+    private static final String WATCH_PROPS_PROP = "etcd.cs.watch";
 
     private static final int DEFAULT_ORDINAL = 1000;
     private static final int DEFAULT_PORT = 2379;
@@ -35,11 +81,15 @@ public class EtcdConfigSource implements ConfigSource, AutoCloseable {
     // Fields
     private static final Logger LOGGER = LoggerFactory.getLogger(EtcdConfigSource.class);
 
+    private final Map<String, String> valueCache = new HashMap<>();
+    private final Map<ByteString, KvClient.Watch> activeWatches = new HashMap<>();
+    private final WatchObserver watchObserver = new WatchObserver();
+
     private int ordinal = 0;
     private String host;
     private int port;
+    private boolean watch;
     private KvStoreClient kvStoreClient;
-    private final Map<String, String> valueCache = new HashMap<>();
 
     // Constructors
 
@@ -55,12 +105,14 @@ public class EtcdConfigSource implements ConfigSource, AutoCloseable {
     public EtcdConfigSource() {
         this.host = ConfigResolver.getPropertyValue(ETCD_HOST_PROP);
         this.port = this.resolveEtcdPort();
+        this.watch = this.resolveWatchProperty();
+
         String user = ConfigResolver.getPropertyValue(ETCD_USER_PROP);
         String password = ConfigResolver.getPropertyValue(ETCD_PASSWORD_PROP);
         LOGGER.debug("etcd host = {}, etcd port = {}, etcd user = {}, etcd password = {}", this.host, this.port, user, password);
 
         if (this.host != null && (this.port != 0)) {
-            if(Strings.isNullOrEmpty(user) || Strings.isNullOrEmpty(password)) {
+            if (Strings.isNullOrEmpty(user) || Strings.isNullOrEmpty(password)) {
                 LOGGER.debug("Creating etcd KV store client without credentials.");
                 this.kvStoreClient = EtcdClient.forEndpoint(this.host, this.port)
                         .withPlainText()
@@ -68,7 +120,7 @@ public class EtcdConfigSource implements ConfigSource, AutoCloseable {
             } else {
                 LOGGER.debug("Creating etcd KV store client with credentials.");
                 this.kvStoreClient = EtcdClient.forEndpoint(this.host, this.port)
-                        .withCredentials(user,password)
+                        .withCredentials(user, password)
                         .withPlainText()
                         .build();
             }
@@ -80,8 +132,8 @@ public class EtcdConfigSource implements ConfigSource, AutoCloseable {
     /**
      * Constructor for unit testing or non-framework usage.
      *
-     * @param host The name of the node hosting the etcd server.
-     * @param port The etcd server's TCP port.
+     * @param host     The name of the node hosting the etcd server.
+     * @param port     The etcd server's TCP port.
      * @param kvClient An intialized <code>KvStoreClient</code> instance.
      */
     public EtcdConfigSource(String host, int port, KvStoreClient kvClient) {
@@ -89,7 +141,7 @@ public class EtcdConfigSource implements ConfigSource, AutoCloseable {
             throw new IllegalArgumentException("kvClient must not be null.");
         }
 
-        if( Strings.isNullOrEmpty(host)){
+        if (Strings.isNullOrEmpty(host)) {
             throw new IllegalArgumentException("host must not be null.");
         }
 
@@ -119,6 +171,15 @@ public class EtcdConfigSource implements ConfigSource, AutoCloseable {
     }
 
     // Private methods
+
+    /**
+     * Resolves the etcd server's TCP port using DeltaSpike's configuration
+     * mechanism. To set the port, define the <code>etcd.endpoint.port</code>
+     * property and set it to an integer value. If not set, the value defaults
+     * to <code>2379</code>.
+     *
+     * @return The server's TCP port.
+     */
     private int resolveEtcdPort() {
         String strPort = ConfigResolver.getPropertyValue(ETCD_PORT_PROP);
         int port = DEFAULT_PORT;
@@ -134,6 +195,71 @@ public class EtcdConfigSource implements ConfigSource, AutoCloseable {
         }
 
         return port;
+    }
+
+    private boolean resolveWatchProperty(){
+        boolean doWatch = false;
+
+        String strWatch = ConfigResolver.getPropertyValue(WATCH_PROPS_PROP);
+        if (!Strings.isNullOrEmpty(strWatch)) {
+            doWatch = Boolean.parseBoolean(strWatch);
+        }
+
+        LOGGER.info("Config source {} watch for property changes.",doWatch ? "will" : "will not");
+
+        return doWatch;
+    }
+
+    private void cacheValue(String key, String value) {
+        synchronized (this.valueCache) {
+            this.valueCache.put(key, value);
+        }
+    }
+
+    private String readCachedValue(String key) {
+        String value = null;
+
+        synchronized (this.valueCache) {
+            value = this.valueCache.get(key);
+        }
+
+        if (value != null && LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Read value '{}' for key '{}' from cache.", value, key);
+        }
+
+        return value;
+    }
+
+    private void removeCachedValue(String key) {
+        LOGGER.debug("Removing value for key '{}' from cache.");
+
+        synchronized (this.valueCache) {
+            this.valueCache.remove(key);
+        }
+    }
+
+    private void addWatch(KvClient client, ByteString etcdKey) {
+        if( this.watch) {
+            this.removeWatch(etcdKey);
+            synchronized (this.activeWatches) {
+                KvClient.Watch watch = client.watch(etcdKey).start(watchObserver);
+                this.activeWatches.put(etcdKey, watch);
+            }
+            LOGGER.debug("Added etcd watch for key '{}'.", etcdKey.toStringUtf8());
+        }
+    }
+
+    private void removeWatch(ByteString etcdKey) {
+        if( this.watch ) {
+            synchronized (this.activeWatches) {
+                if (this.activeWatches.containsKey(etcdKey)) {
+                    LOGGER.debug("Closing current watch for '{}' and removing from map.", etcdKey.toStringUtf8());
+                    KvClient.Watch active = this.activeWatches.get(etcdKey);
+                    this.activeWatches.remove(etcdKey);
+                    active.close();
+                }
+            }
+        }
     }
 
     // ConfigSource
@@ -177,21 +303,19 @@ public class EtcdConfigSource implements ConfigSource, AutoCloseable {
 
         if (this.kvStoreClient != null) {
             try {
-                synchronized (this.valueCache) {
-                    value = this.valueCache.get(key);
-                }
-
+                value = this.readCachedValue(key);
                 if (value == null) {
                     KvClient client = this.kvStoreClient.getKvClient();
-                    RangeResponse response = client.get(ByteString.copyFromUtf8(key)).sync();
+                    ByteString etcdKey = ByteString.copyFromUtf8(key);
+
+                    RangeResponse response = client.get(etcdKey).sync();
 
                     if (response.getCount() > 0) {
                         value = response.getKvs(0).getValue().toStringUtf8();
 
                         if (value != null) {
-                            synchronized (this.valueCache) {
-                                this.valueCache.put(key, value);
-                            }
+                            this.cacheValue(key, value);
+                            this.addWatch(client, etcdKey);
                         }
                     }
                 } else {
@@ -227,7 +351,22 @@ public class EtcdConfigSource implements ConfigSource, AutoCloseable {
      */
     @Override
     public void close() throws IOException {
+        if (this.watch && this.activeWatches.size() > 0) {
+            LOGGER.debug("Closing all active watches.");
+            for (KvClient.Watch watch : this.activeWatches.values()) {
+                try {
+                    watch.close();
+                } catch (Exception e) {
+                    LOGGER.info("Error closing watch stream: {}", e.getMessage());
+                }
+            }
+        }
+
         LOGGER.debug("Closing KV store client.");
-        this.kvStoreClient.close();
+        try {
+            this.kvStoreClient.close();
+        } catch (Exception e) {
+            LOGGER.info("Error closing KV Store client: ",e.getMessage());
+        }
     }
 }
--- src/test/java/net/ghettopalace/etcd/EtcdConfigSourceIT.java
+++ src/test/java/net/ghettopalace/etcd/EtcdConfigSourceIT.java
@@ -16,22 +16,25 @@ class EtcdConfigSourceIT {
     private static final String ETCD_PORT = "4001";
     private static final String TEST_KEY = "test.key";
     private static final String TEST_VALUE = "IT Value";
+    private static final String CHANGED_TEST_VALUE = "Changed IT Value";
     private static final String ETCD_ENDPOINT_HOST_PROPERTY = "etcd.endpoint.host";
     private static final String ETCD_ENDPOINT_PORT_PROPERTY = "etcd.endpoint.port";
     private static final String ETCD_USER_PROP = "etcd.endpoint.user";
     private static final String ETCD_PASSWORD_PROP = "etcd.endpoint.password";
     private static final String ETCD_USER = "root";
     private static final String ETCD_PASSWORD = "Passw0rd";
+    public static final String ETCD_CS_WATCH_PROP = "etcd.cs.watch";
 
     // Tests
     @BeforeAll
     static void initializeTests() throws Exception {
         System.setProperty(ETCD_ENDPOINT_HOST_PROPERTY, ETCD_HOST);
         System.setProperty(ETCD_ENDPOINT_PORT_PROPERTY, ETCD_PORT);
+        System.setProperty(ETCD_CS_WATCH_PROP, "");
 
         // Create test key(s)
         try (KvStoreClient kvStoreClient = EtcdClient.forEndpoint(ETCD_HOST, Integer.parseInt(ETCD_PORT))
-                .withCredentials("root", "Passw0rd")
+                .withCredentials(ETCD_USER, ETCD_PASSWORD)
                 .withPlainText()
                 .build()) {
 
@@ -83,6 +86,65 @@ class EtcdConfigSourceIT {
 
         try (EtcdConfigSource source = new EtcdConfigSource()) {
             String value = source.getPropertyValue(TEST_KEY);
+            assertThat(value).isNotNull()
+                    .isEqualTo(TEST_VALUE);
+        }
+    }
+
+    @Test
+    @DisplayName("Read Changed Configuration Value")
+    void readChangedConfigurationValue() throws Exception {
+        System.setProperty(ETCD_USER_PROP, ETCD_USER);
+        System.setProperty(ETCD_PASSWORD_PROP, ETCD_PASSWORD);
+        System.setProperty(ETCD_CS_WATCH_PROP, "true");
+
+        try (EtcdConfigSource source = new EtcdConfigSource()) {
+            String value = source.getPropertyValue(TEST_KEY);
+            assertThat(value).isNotNull()
+                    .isEqualTo(TEST_VALUE);
+
+            try (KvStoreClient kvStoreClient = EtcdClient.forEndpoint(ETCD_HOST, Integer.parseInt(ETCD_PORT))
+                    .withCredentials(ETCD_USER, ETCD_PASSWORD)
+                    .withPlainText()
+                    .build()) {
+
+                KvClient client = kvStoreClient.getKvClient();
+                client.put(ByteString.copyFromUtf8(TEST_KEY), ByteString.copyFromUtf8(CHANGED_TEST_VALUE)).sync();
+            }
+
+            // Sleep for 5 seconds; watches are process asynchronously
+            Thread.sleep(5000);
+
+            value = source.getPropertyValue(TEST_KEY);
+            assertThat(value).isNotNull()
+                    .isEqualTo(CHANGED_TEST_VALUE);
+        }
+    }
+
+    @Test
+    @DisplayName("Ignore Changed Configuration Value")
+    void ignoreChangedConfigurationValue() throws Exception {
+        System.setProperty(ETCD_USER_PROP, ETCD_USER);
+        System.setProperty(ETCD_PASSWORD_PROP, ETCD_PASSWORD);
+
+        try (EtcdConfigSource source = new EtcdConfigSource()) {
+            String value = source.getPropertyValue(TEST_KEY);
+            assertThat(value).isNotNull()
+                    .isEqualTo(TEST_VALUE);
+
+            try (KvStoreClient kvStoreClient = EtcdClient.forEndpoint(ETCD_HOST, Integer.parseInt(ETCD_PORT))
+                    .withCredentials(ETCD_USER, ETCD_PASSWORD)
+                    .withPlainText()
+                    .build()) {
+
+                KvClient client = kvStoreClient.getKvClient();
+                client.put(ByteString.copyFromUtf8(TEST_KEY), ByteString.copyFromUtf8(CHANGED_TEST_VALUE)).sync();
+            }
+
+            // Sleep for 5 seconds; watches are process asynchronously
+            Thread.sleep(5000);
+
+            value = source.getPropertyValue(TEST_KEY);
             assertThat(value).isNotNull()
                     .isEqualTo(TEST_VALUE);
         }
